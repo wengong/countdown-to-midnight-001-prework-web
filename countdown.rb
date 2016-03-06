869e2157d26204e5a58d65d50a130847e8b713ca
wengong <wentoshare@gmail.com>
2016-03-06 21:00:03 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,27 @@
 #write your code here
 
-def countdown
+def countdown(arg)
+  while arg>0 do
+    puts"#{arg} SECOND(S)!"
+    arg-=1
+
+
+  end
+  "HAPPY NEW YEAR!"
+end
+
+
+
+
+
+
+
+
+def countdown_with_sleep(arg)
+  while arg>0
+    puts"#{arg}"
+    sleep(1)
+    arg-=1
+
+  end
 end
